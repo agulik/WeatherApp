81073073068e5c86d83a6a9deb1aab70499f1586
Andrew Gulik <andrewgulik@gmail.com>
2017-02-22 12:36:06 +0800

    mapped out call stack process for js file, added some comments

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
+// 1). create on enter event for city name typed in (done)
+
+// 2). create function to receive the city entered as a value
+
+// 3). create function to convert passed in city to lat/long values (semi-done)
+
+// 4). use those values to plug into end of api call
+
+// 5). retrieve weather data from api
+
+
+
+// function to convert City to Latitude and Longitude
+
+var getLocation = function(address) {
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode( {'address': address}, function(results, status) {
+
+    if (status == google.maps.GeocoderStatus.OK) {
+      var latitude = results[0].geometry.location.lat();
+      var longitude = results[0].geometry.location.lng();
+      console.log(latitude, longitude); // this is the lat and lng of Montreal
+    }
+  });
+}
+
+getLocation('Montreal');
+
+
+
+/*
 
 // create a variable for the weather API
 
-var weatherApi = "http://api.openweathermap.org/data/2.5/forecast/city?&APPID=662cad70ecf564015d19a592ab74408b";
+// include "/latitude,longitude" at end of api call
+
+var weatherApi = "https://api.darksky.net/forecast/53fdce807cb09b72e9a6318f4643f6b3"
+
 
 // add event listener for pressing enter
 // if the enter key is pressed, it fires off the getWeather function
@@ -34,6 +68,6 @@ function getWeather() {
 
 }
 
+*/
 
-// need to add an onclick event for weather icon top left
-// if clicked, refresh page back to initial load
+// need to add
