b3c8536dd1d8bf3cac62f3a13ea0fccea927aa55
Andrew Gulik <andrewgulik@gmail.com>
2017-02-23 19:53:34 +0800

    cleaned up the javascript code

--- js/main.js
+++ js/main.js
@@ -1,47 +1,38 @@
-// 1). create on enter event for city name typed in (done)
-
-// 2). create function to receive the city entered as a value
-
-// 3). create function to convert passed in city to lat/long values (semi-done)
-
-// 4). use those values to plug into end of api call
-
-// 5). retrieve weather data from api
 
 
 // event listener for pressing enter
 
 $("#txt-input").keydown(function (e) {
+
   var key = e.which;
+
   if (key === 13) {
-    getWeather();
+
+    // declare global variable for input value
+
+    var inputValue = document.getElementById('txt-input').value;
+
+    console.log(inputValue);
+
   }
 })
 
-// getWeather function to take input value, convert to lat/lng and get req api
+// function that converts city name to lat/lng values
 
+function getLocation(address) {
 
-function getWeather() {
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode( {'address': address}, function(results, status) {
 
-  // helper function to convert City to Latitude and Longitude
+    if (status == google.maps.GeocoderStatus.OK) {
+      var latitude = results[0].geometry.location.lat();
+      var longitude = results[0].geometry.location.lng();
+      console.log(latitude,longitude); // this is the lat and lng of Montreal
+    }
+  });
+}
 
-  var getLocation = function(address) {
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode( {'address': address}, function(results, status) {
-
-      if (status == google.maps.GeocoderStatus.OK) {
-        var latitude = results[0].geometry.location.lat();
-        var longitude = results[0].geometry.location.lng();
-        console.log(latitude,longitude); // this is the lat and lng of Montreal
-      }
-    });
-  }
-
-  var inputValue = $("#txt-input").val();
-
-  getLocation(inputValue);
-
-  }
+getLocation('Malbaie');
 
 
 
