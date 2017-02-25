0ecc862a38ed2ac1cce683ffbd615f15ac7076f2
Andrew Gulik <andrewgulik@gmail.com>
2017-02-25 12:15:58 +0800

    added eslint, played with function scope

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
 
+// declaire global input variable
+
+var inputValue = document.getElementById("txt-input");
 
 // event listener for pressing enter
 
-$("#txt-input").keydown(function (e) {
 
-  var key = e.which;
 
-  if (key === 13) {
-
-    var inputValue = document.getElementById('txt-input').value;
-
-    console.log(inputValue);
-
-  }
-})
 
 // function for converting city name to lat/lng values
 
 
 function getLocation(address, callback) {
 
+  inputValue.addEventListener("keydown", function(e) {
+
+    if (e.keyCode === 13) {
+
+      console.log(inputValue.value);
+
+    }
+  });
+  
   var geocoder = new google.maps.Geocoder();
 
-  geocoder.geocode( {'address': address}, function(results, status) {
+  geocoder.geocode( {"address": address}, function(results, status) {
 
     if (status == google.maps.GeocoderStatus.OK) {
       var latitude = results[0].geometry.location.lat();
@@ -32,13 +34,15 @@ function getLocation(address, callback) {
   });
 }
 
-getLocation('Copenhagen', function(data) {
+getLocation("Copenhagen", function(data) {
   console.log(data);
 });
+
+
 
 
   // create a variable for the weather API
 
   // include "/latitude,longitude" at end of api call
 
-  var weatherApi = "https://api.darksky.net/forecast/53fdce807cb09b72e9a6318f4643f6b3"
+  var weatherApi = "https://api.darksky.net/forecast/53fdce807cb09b72e9a6318f4643f6b3";
