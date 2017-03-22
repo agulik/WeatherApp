f0299809969102a2a9310e2967ec19f50b7426d8
Andrew Gulik <andrewgulik@gmail.com>
2017-03-22 13:42:13 -0400

    updating some linting rules, continued to work with the js file

--- js/main.js
+++ js/main.js
@@ -1,43 +1,40 @@
-// I need to add the geocode function to the scrope of the text inputValue
+// I need to add the geocode function to the scope of the text inputValue
 
 // declaire global input variable
 
 var inputValue = document.getElementById("txt-input");
 
-
 // function for converting city name to lat/lng values
 
+  inputValue.addEventListener("keydown", function(e) {
+    if (e.keyCode === 13) {
+      console.log(inputValue.value);
+      }
+});
 
-function getLocation(address, callback) {
+var getLocation =  function(address) {
 
   var geocoder = new google.maps.Geocoder();
 
-  inputValue.addEventListener("keydown", function(e) {
-
-    if (e.keyCode === 13) {
-
-      console.log(inputValue.value);
-
-    }
-  });
-
-  geocoder.geocode( {"address": address}, function(results, status) {
+  geocoder.geocode( { "address": address}, function(results, status) {
 
     if (status == google.maps.GeocoderStatus.OK) {
       var latitude = results[0].geometry.location.lat();
       var longitude = results[0].geometry.location.lng();
-      callback({ Address: address, Latitude: latitude, Longitude: longitude });
     }
+     console.log(latitude, longitude);
   });
-}
+};
 
-getLocation("Paris", function(data) {
-  console.log(data);
-});
+getLocation("Copenhagen");
+
+
+
+
 
 
   // create a variable for the weather API
 
   // include "/latitude,longitude" at end of api call
 
-  var weatherApi = "https://api.darksky.net/forecast/53fdce807cb09b72e9a6318f4643f6b3";
+//  var weatherApi = "https://api.darksky.net/forecast/53fdce807cb09b72e9a6318f4643f6b3";
