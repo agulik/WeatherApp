391c50cdbbc77f3c77b2d41b58ca43a90d4cadbf
Andrew Gulik <andrewgulik@gmail.com>
2017-03-22 14:16:31 -0400

    made changes to js file

--- js/main.js
+++ js/main.js
@@ -1,40 +1,45 @@
-// I need to add the geocode function to the scope of the text inputValue
 
 // declaire global input variable
 
 var inputValue = document.getElementById("txt-input");
+var geocoder;
+var google;
 
-// function for converting city name to lat/lng values
+// initialize google map geocode function
 
-  inputValue.addEventListener("keydown", function(e) {
-    if (e.keyCode === 13) {
-      console.log(inputValue.value);
-      }
-});
+function initializeMap() {
+  geocoder = new google.maps.Geocoder();
+}
 
-var getLocation =  function(address) {
+// converting input to lat/lng coordinates function
 
-  var geocoder = new google.maps.Geocoder();
-
-  geocoder.geocode( { "address": address}, function(results, status) {
-
-    if (status == google.maps.GeocoderStatus.OK) {
+function convertAddress(address) {
+  var address = document.getElementById("txt-input").value;
+  geocoder.geocode({ "Address": address }, function(results, status) {
+    if (status === google.maps.Geocoder.OK) {
       var latitude = results[0].geometry.location.lat();
       var longitude = results[0].geometry.location.lng();
     }
      console.log(latitude, longitude);
-  });
-};
+    }
+)}
 
-getLocation("Copenhagen");
+// press-enter event listener which calls convertAddress function onpress
+
+  inputValue.addEventListener("keydown", function(e) {
+    if (e.keyCode === 13) {
+        console.log("yea..this works..!");
+        convertAddress();
+      }
+});
+
+//initialize the map on page load
+
+initializeMap();
 
 
+// create a variable for the weather API
 
+// include "/latitude,longitude" at end of api call
 
-
-
-  // create a variable for the weather API
-
-  // include "/latitude,longitude" at end of api call
-
-//  var weatherApi = "https://api.darksky.net/forecast/53fdce807cb09b72e9a6318f4643f6b3";
+// var weatherApi = "https://api.darksky.net/forecast/53fdce807cb09b72e9a6318f4643f6b3";
