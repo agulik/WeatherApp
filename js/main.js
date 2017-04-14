4f9176d7f9ae1e18619425706233b4035654e678
Andrew Gulik <andrewgulik@gmail.com>
2017-04-13 21:14:27 -0400

    fixed everything, reverted back to old api call

--- js/main.js
+++ js/main.js
@@ -35,17 +35,11 @@ function convertAddress(address, callback) {
   });
 }
 
-
-//initialize the map on page load
-
 initializeMap();
 
 convertAddress("Copenhagen", function(data) {
   console.log(data);
 });
-
-
-
 
 // create a variable for the weather API
 
